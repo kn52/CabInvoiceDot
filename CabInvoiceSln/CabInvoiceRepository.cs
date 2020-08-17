a5cb0ac97ee76f7c430d13c2c0f2fbc61185228d
kn52 <ashish52922@gmail.com>
2020-08-17 23:50:40 +0530

    [Aashish]ADDED:Cab repository class to add cab rides

---
+++ CabInvoiceSln/CabInvoiceRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceSln
+{
+    public class CabInvoiceRepository
+    {
+        Dictionary<string, List<Ride>> user = new Dictionary<string, List<Ride>>();
+
+        public void addRides(String userId, Ride[] ride)
+        {
+            List<Ride> rides = this.user[userId];
+            if (rides == null)
+                user.Add(userId, new List<Ride>(ride));
+        }
+
+        public Ride[] getRides(String userId)
+        {
+            return user[userId].ToArray();
+        }
+    }
+}
