1255a930ead453c2f8b76ebe0ed33b07a1c840bc
kn52 <ashish52922@gmail.com>
2020-08-17 23:26:38 +0530

    [Aashish]ADDED:Summary class to get average fare

---
+++ CabInvoiceSln/CabInvoiceSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceSln
+{
+    public class CabInvoiceSummary
+    {
+        public double averageFare;
+        public double numberOfRides;
+        public double totalFare;
+
+        public CabInvoiceSummary(double numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = totalFare / numberOfRides;
+        }
+    }
+}
