2a92167bf23e0a09308284f6111abd7031b201a7
kn52 <ashish52922@gmail.com>
2020-08-17 22:43:51 +0530

    [Aashish]ADDED:Test to return total fare on given distance and time

--- CabInvoiceTest/CabInvoiceTest.cs
+++ CabInvoiceTest/CabInvoiceTest.cs
@@ -5,15 +5,21 @@ namespace Tests
 {
     public class Tests
     {
+        CabInvoice cabInvoice;
+
         [SetUp]
         public void Setup()
         {
+            cabInvoice = new CabInvoice();
         }
 
         [Test]
         public void WhenGiven_DistanceAndTime_ShouldReturn_TotalFare()
         {
-            Assert.Pass();
+            double distance = 2;
+            int time = 1;
+            double totalFare = cabInvoice.CalculateFare(distance, time);
+            Assert.AreEqual(21,totalFare);
         }
     }
 }
