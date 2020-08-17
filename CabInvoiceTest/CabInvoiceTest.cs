fa9f5c767ad89dc86916c61cd9e4b7f812449e44
kn52 <ashish52922@gmail.com>
2020-08-17 22:40:57 +0530

    [Aashish]ADDED:Function to calculate total fare of ride

---
+++ CabInvoiceTest/CabInvoiceTest.cs
@@ -0,0 +1,19 @@
+using CabInvoiceSln;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenGiven_DistanceAndTime_ShouldReturn_TotalFare()
+        {
+            Assert.Pass();
+        }
+    }
+}
